6f5316dcceb501d56d8ceba46c5500cbb7aba861
EthanLiu2 <etliu1@s.sfusd.edu>
2025-01-22 09:35:40 -0800

    Update PigLatin.java

--- src/main/java/PigLatin.java
+++ src/main/java/PigLatin.java
@@ -27,6 +27,14 @@ public class PigLatin {
 	        System.out.println(pigLatin(lines[i]));
 	    }
     }
+	public int findFirstVowel(String sWord) {
+  for (int i=0; i<sWord.length(); i++) {
+    if (sWord.substring(i, i+1).equals("a")||sWord.substring(i, i+1).equals("e")||sWord.substring(i, i+1).equals("i")||sWord.substring(i, i+1).equals("o")||sWord.substring(i, i+1).equals("u")) {
+      return i;
+    }
+  }
+  return -1;
+}
     public String pigLatin(String sWord) {
   //precondition: sWord is a valid String of length greater than 0
   //postcondition: returns the pig latin equivalent of sWord
